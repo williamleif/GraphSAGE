1740c0c37088deff392c041d9468c5669772ac3e
Amir Abbasinejad <a.abasinejad@gmail.com>
2020-03-27 00:47:02 +0100

    Update utils.py

--- graphsage/utils.py
+++ graphsage/utils.py
@@ -82,7 +82,7 @@ def run_random_walks(G, nodes, num_walks=N_WALKS):
         for i in range(num_walks):
             curr_node = node
             for j in range(WALK_LEN):
-                next_node = random.choice(G.neighbors(curr_node))
+                next_node = random.choice(list(G.neighbors(curr_node)))
                 # self co-occurrences are useless
                 if curr_node != node:
                     pairs.append((node,curr_node))
