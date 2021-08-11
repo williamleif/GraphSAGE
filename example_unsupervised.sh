758d80625eda287419ab919a71e568e25ee74da6
chihming <changecandy@gmail.com>
2021-08-12 02:46:45 +0800

    modify the example file name

--- example_unsupervised.sh
+++ example_unsupervised.sh
@@ -1,1 +1,1 @@
-python -m graphsage.unsupervised_train --train_prefix ./example_data/ppi --model graphsage_mean --max_total_steps 1000 --validate_iter 10
+python -m graphsage.unsupervised_train --train_prefix ./example_data/toy-ppi --model graphsage_mean --max_total_steps 1000 --validate_iter 10
